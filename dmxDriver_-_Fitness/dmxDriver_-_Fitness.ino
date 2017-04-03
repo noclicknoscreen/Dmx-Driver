18cc520584d67fb2df24951123aaf7ec0dd85463
Dudley smith <vj.dudley.smith@gmail.com>
2017-04-03 18:21:00 +0200

    First step (Basics + more advanced)

---
+++ dmxDriver_-_Fitness/dmxDriver_-_Fitness.ino
@@ -0,0 +1,104 @@
+/* Welcome to DmxSimple. This library allows you to control DMX stage and
+** architectural lighting and visual effects easily from Arduino. DmxSimple
+** is compatible with the Tinker.it! DMX shield and all known DIY Arduino
+** DMX control circuits.
+**
+** DmxSimple is available from: http://code.google.com/p/tinkerit/
+** Help and support: http://groups.google.com/group/dmxsimple       */
+
+/* To use DmxSimple, you will need the following line. Arduino will
+** auto-insert it if you select Sketch > Import Library > DmxSimple. */
+
+#include <DmxSimple.h>
+
+/* DMX Declaration section
+   Basics DMX settings
+*/
+#define pinDmx      3
+
+void dmxSetup() {
+  DmxSimple.usePin(pinDmx);
+  DmxSimple.maxChannel(nbContours + nbVolumes);
+}
+
+/* ------------------------------------------
+    Specific jean mone stuff
+    Contours, Volumes
+*/
+#define nbContours 2
+#define nbVolumes  2
+
+int contoursChannels[nbContours] = {1, 4};
+int volumesChannels[nbVolumes] = {2, 3};
+
+/* ---------------------------------------
+    Iterates Contours
+  --------------------------------------- */
+void lightContours(int brightness) {
+  // Iterates channels --
+  for (int idx = 0; idx < nbContours; idx++) {
+    lightChannel(contoursChannels[idx], brightness);
+  }
+}
+
+/* ---------------------------------------
+    Iterates Volumes
+  --------------------------------------- */
+void lightVolumes(int brightness) {
+  // Iterates each channels of volumes setup --
+  for (int idx = 0; idx < nbVolumes; idx++) {
+    lightChannel(volumesChannels[idx], brightness);
+  }
+}
+
+/* ---------------------------------------------
+    Root function (Sending DMX)
+  --------------------------------------------- */
+void lightChannel(int channel, int brightness) {
+  /* Update DMX channel 1 to new brightness */
+  DmxSimple.write(channel, brightness);
+
+  /* Log brightness written ---- */
+  Serial.print("Brightness [");
+  Serial.print(brightness);
+  Serial.print("] send to channel [");
+  Serial.print(channel);
+  Serial.print("]");
+  Serial.println();
+
+}
+/* ----------------------------------------- */
+
+
+/* ---------------------------------------------------------
+ *  MAIN SECTION : Setup + Loop
+ */
+void setup() {
+  // Log setup
+  Serial.begin(9600);
+
+  // DMX protocol setup
+  dmxSetup();
+
+}
+
+
+void loop() {
+
+  int brightness;
+
+  /* Simple loop to ramp up brightness */
+  for (brightness = 0; brightness <= 255; brightness++) {
+
+    /* Sending Contours (Static) */
+    lightContours(255);
+
+    /* Sending Volumes (Moving) */
+    lightVolumes(brightness);
+
+    /* Small delay to slow down the ramping */
+    delay(10);
+
+  }
+
+}
