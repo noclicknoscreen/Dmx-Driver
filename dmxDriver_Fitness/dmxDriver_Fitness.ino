a11bf134dc7ca901e07053a22f354e8b122c279c
Dudley smith <vj.dudley.smith@gmail.com>
2017-04-05 15:07:01 +0200

    Adding push button

--- dmxDriver_Fitness/dmxDriver_Fitness.ino
+++ dmxDriver_Fitness/dmxDriver_Fitness.ino
@@ -24,20 +24,25 @@ using namespace std;
 /* ANIMATION SECTION */
 animation volumeValue;
 animation contourValue;
+vector<animation> multipleAnims;
 
 int scenario = 0;
 
 #define scenFadeIn  0
-#define scenStill   1
+#define scenSin   1
 #define scenWaveA   2
 #define scenWaveRandom   3
-#define scenRandom  4
+#define scenMultiple  4
 #define scenFadeOut 5
+#define MAX_SCENARIO 5
+
+#define minBright 0
+#define maxBright 150
 
 long period = 5000;
 
 int waveRandomIndex = 0;
-int waveRandomBrightness = 255;
+int waveRandomBrightness = maxBright;
 
 // Serial read and test instructions ----
 String inputString = "";         // a string to hold incoming data
@@ -50,21 +55,30 @@ boolean stringComplete = false;  // whether the string is complete
 vector<int> contoursChannels = {1};
 vector<int> volumesChannels = {2, 3, 4};
 
+/*
+   BUTTON SECTION
+*/
+const int buttonPin = 2;    // the number of the pushbutton pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 /* ---------------------------------------
     Iterates channels
   --------------------------------------- */
 void initScenario() {
+
   // Animation section
   contourValue.start(period);
   volumeValue.start(period);
 
   // Specific inits procedure
-  /* Sending Contours (Static) */
   switch (scenario) {
     case scenFadeIn:
       break;
 
-    case scenStill:
+    case scenSin:
       break;
 
     case scenWaveA:
@@ -74,7 +88,8 @@ void initScenario() {
       waveRandomIndex = getNewIndexwaveRandom();
       break;
 
-    case scenRandom:
+    case scenMultiple:
+      restartMultipleAnims();
       break;
 
     case scenFadeOut:
@@ -94,6 +109,10 @@ void initScenario() {
   Serial.println();
 
 }
+
+/*
+   SCENARII SECTION ----------------------------------------------------------
+*/
 void simple(vector<int> _channels, int _brightness) {
   // Iterates channels --
   for (int idx = 0; idx < _channels.size(); idx++) {
@@ -107,7 +126,7 @@ void waveA(vector<int> _channels) {
   for (int idx = 0; idx < _channels.size(); idx++) {
 
     float wavePhase = animation::floatMap(idx, 0.0f, _channels.size(), 0.0f, 0.25f);
-    float brightness = volumeValue.loopSin(wavePhase, 0, 255);
+    float brightness = volumeValue.loopSin(wavePhase, minBright, maxBright);
 
     // each channel has its phase --
     lightChannel(_channels[idx], brightness);
@@ -116,12 +135,11 @@ void waveA(vector<int> _channels) {
 }
 
 void waveRandom(vector<int> _channels) {
-
   // Iterates channels --
   for (int idx = 0; idx < _channels.size(); idx++) {
 
     if (idx == waveRandomIndex) {
-      float brightness = volumeValue.loopSin(0, 0, 255);
+      float brightness = volumeValue.loopSin(0, minBright, maxBright);
       lightChannel(_channels[idx], brightness);
 
       // Change the index if lightness is reach
@@ -135,9 +153,52 @@ void waveRandom(vector<int> _channels) {
 
 }
 
+void multiple(vector<int> _channels) {
+  // Iterates channels --
+  for (int idx = 0; idx < _channels.size(); idx++) {
+    if (idx < multipleAnims.size()) {
+
+      // -- Each channel has a different animation
+      float animValue =  multipleAnims[idx].loopSin(0, minBright, maxBright);
+      lightChannel(_channels[idx], animValue);
+
+    } else {
+      // -- Default is on
+      lightChannel(_channels[idx], maxBright);
+
+    }
+  }
+
+}
+
 int getNewIndexwaveRandom() {
   return random(0, volumesChannels.size());;
 }
+
+void restartMultipleAnims() {
+  for (int idx = 0; idx < multipleAnims.size(); idx++) {
+    long newPeriod = random(0.5 * period, 1.5 * period);
+    multipleAnims[idx].start(newPeriod);
+
+    // -- log it
+    Serial.print("Restarting animations [");
+    Serial.print(idx);
+    Serial.print("] with a new period [");
+    Serial.print(newPeriod);
+    Serial.print("]");
+    Serial.println();
+
+  }
+}
+void updateMultipleAnims() {
+  for (int idx = 0; idx < multipleAnims.size(); idx++) {
+    multipleAnims[idx].update();
+  }
+}
+/*
+   End of scenarii section
+   -------------------------------------------------------------------
+*/
 
 /* ---------------------------------------------
     Root function (Sending DMX)
@@ -145,16 +206,6 @@ int getNewIndexwaveRandom() {
 void lightChannel(int channel, long brightness) {
   /* Update DMX channel 1 to new brightness */
   DmxSimple.write(channel, brightness);
-
-  /* Log brightness written ---- */
-  /*
-    Serial.print("Brightness [");
-    Serial.print(brightness);
-    Serial.print("] send to channel [");
-    Serial.print(channel);
-    Serial.print("]");
-    Serial.println();
-  */
 }
 /* ----------------------------------------- */
 
@@ -167,13 +218,24 @@ void setup() {
   // Log setup
   Serial.begin(9600);
 
+  // Button pin setup
+  pinMode(buttonPin, INPUT_PULLUP);
+
   // DMX protocol setup
   dmxSetup();
 
   // Animation section
-  initScenario();
+  // Init multiple animations array
+  for (int idx = 0; idx < volumesChannels.size(); idx++) {
+    animation newAnim;
+    multipleAnims.push_back(newAnim);
+  }
+  // Starting them
+  restartMultipleAnims();
 
+  // Scenario section
   scenario = 0;
+  initScenario();
 
 }
 
@@ -189,51 +251,85 @@ void loop() {
 
   contourValue.update();
   volumeValue.update();
+  updateMultipleAnims();
+
+  readingButton();
 
   /* Sending Contours (Static) */
   switch (scenario) {
 
     case scenFadeIn:
       // FADE IN and still
-      simple(contoursChannels, contourValue.fadeIn(0, 0, 255));
-      simple(volumesChannels, volumeValue.fadeIn(0, 0, 255));
+      simple(contoursChannels, contourValue.fadeIn(0, 0, maxBright));
+      simple(volumesChannels, volumeValue.fadeIn(0, 0, maxBright));
       break;
 
-    case scenStill:
+    case scenSin:
       // Sinus Lights
-      simple(contoursChannels, 255);
-      simple(volumesChannels, volumeValue.loopSin(0, 0, 255));
+      simple(contoursChannels, maxBright);
+      simple(volumesChannels, volumeValue.loopSin(0, minBright, maxBright));
       break;
 
     case scenWaveA:
       // Waving A spatially
-      simple(contoursChannels, 255);
+      simple(contoursChannels, maxBright);
       waveA(volumesChannels);
       break;
 
     case scenWaveRandom:
       // Waving B spatially
-      simple(contoursChannels, 255);
+      simple(contoursChannels, maxBright);
       waveRandom(volumesChannels);
       break;
 
-    case scenRandom:
+    case scenMultiple:
       // FADE OUT
-      simple(contoursChannels, contourValue.fadeOut(0, 0, 255));
-      simple(volumesChannels, volumeValue.fadeOut(0, 0, 255));
+      simple(contoursChannels, maxBright);
+      multiple(volumesChannels);
       break;
 
     case scenFadeOut:
       // FADE OUT
-      simple(contoursChannels, contourValue.fadeOut(0, 0, 255));
-      simple(volumesChannels, volumeValue.fadeOut(0, 0, 255));
+      simple(contoursChannels, contourValue.fadeOut(0, 0, maxBright));
+      simple(volumesChannels, volumeValue.fadeOut(0, 0, maxBright));
       break;
 
     default:
-      simple(contoursChannels, 255);
-      simple(volumesChannels, 255);
+      simple(contoursChannels, maxBright);
+      simple(volumesChannels, maxBright);
 
   }
+
+}
+
+void readingButton() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == LOW) {
+        plusScenario();
+      }
+    }
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
 
 }
 
@@ -243,11 +339,21 @@ String scenarioName(int _scenario) {
     case 0: return "Fade in";
     case 1: return "Still";
     case 2: return "Wave A";
-    case 3: return "Wave B";
-    case 4: return "Random";
+    case 3: return "Wave Random";
+    case 4: return "Multiple";
     case 5: return "Fade Out";
     default: return "No name";
   }
+}
+
+void plusScenario() {
+  scenario++;
+  if (scenario > MAX_SCENARIO) {
+    scenario = 0;
+  }
+
+  initScenario();
+  
 }
 
 /*
